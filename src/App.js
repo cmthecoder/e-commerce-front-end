579fd8da9ff5601444a1150f6470e61a2fb61fae
Chris Mems <christopher.mems@.com>
2023-04-08 12:29:23 -0500

    set up theme

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Category from './pages/Category';
-import Product from './pages/Product';
-
 function App() {
   return (
-    <>
-      <Routes>
-        <Route
-          path='/'
-          element={<Home />}
-        />
-        <Route
-          path='/products/:id'
-          element={<Category />}
-        />
-        <Route
-          path='/product/:id'
-          element={<Product />}
-        />
-      </Routes>
-    </>
+    <div className="app">
+      
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { theme } from "./theme";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline>
+        <Router>
+          <App />
+        </Router>
+      </CssBaseline>
+    </ThemeProvider>
   </React.StrictMode>
 );
