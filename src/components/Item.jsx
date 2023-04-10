9a2f359067d0ec9def32fd72cd3cc4abb9f9189d
Chris Mems <christopher.mems@.com>
2023-04-09 19:49:27 -0500

    create the UI for the items

---
+++ src/components/Item.jsx
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { IconButton, Box, Typography, useTheme, Button } from "@mui/material";
+import AddIcon from "@mui/material/Add";
+import Remove from "@mui/icons-material/Remove";
+import { shades } from "../theme";
+import { addToCart } from "../state";
+import { useNavigate } from "react-router-dom";
+
+const Item = ({ item, width }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [count, setCount] = useState(1);
+  const [isHovered, setIsHovered] = useState(false);
+  const {
+    palete: { netrual },
+  } = useTheme();
+
+  const { category, price, name, image } = item.attributes;
+  const {
+    data: {
+      attributes: {
+        formats: {
+          medium: { url },
+        },
+      },
+    },
+  } = image;
+
+  return (
+    <Box width={width}>
+      <Box
+        position="relative"
+        onMouseOver={() => setIsHovered(true)}
+        onMouseOut={() => setIsHovered(false)}
+      >
+        <img
+          src={`http://localhost:1337${url}`}
+          alt={item.name}
+          width="300px"
+          height="400px"
+          onClick={() => navigate(`/item/${item.id}`)}
+        />
+      </Box>
+    </Box>
+  );
+};
+
+export default Item;
